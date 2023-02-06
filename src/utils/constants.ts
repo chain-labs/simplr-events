f2549f46893a88d7439cc56e6355e55fd255e29b
Angel Lakra <angel.lakra@chainlabs.in>
2023-02-06 22:36:56 +0530

    replaced Simplr logo
    Signed-off-by: Angel Lakra <angel.lakra@chainlabs.in>

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const LOGO_URL =
-  'https://ik.imagekit.io/chainlabs/Simplr_Collection_Dapp/simplr_logo-min_eO5qws5Lt.png?ik-sdk-version=javascript-1.4.3&updatedAt=1670313829366'
+  'https://ik.imagekit.io/chainlabs/Simplr_Collection_Dapp/simplr-logo__v0Tmlq6M.png?ik-sdk-version=javascript-1.4.3&updatedAt=1675703009631'
 export const BACKGROUND_MEDIA =
   'https://ik.imagekit.io/chainlabs/Simplr_Events/pexels-adrien-olichon-2387793_7h4CeXjRss.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1675402350598'
 export const SUBGRAPH_ENDPOINT = process.env.NEXT_PUBLIC_SUBGRAPH_ENDPOINT
