68a71f131e7f64b7013ed2f4fb1bda008aaf47db
Angel Lakra <angel.lakra@chainlabs.in>
2023-02-17 20:55:10 +0530

    added plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,12 @@ export default function App({ Component, pageProps }: AppProps) {
           rel="shortcut icon"
           href="https://ik.imagekit.io/chainlabs/Simplr_Collection_Dapp/Simplr_Dark_Logo__kMoJpXxz.svg?ik-sdk-version=javascript-1.4.3&updatedAt=1676563761000"
         />
+        <script
+          defer
+          data-domain="vivacity23.simplrhq.com"
+          src="https://plausible.io/js/script.js"
+        ></script>
+
         <title>Simpr Events | Vivacity 2023 - Admin</title>
       </Head>
       <Component {...pageProps} />
