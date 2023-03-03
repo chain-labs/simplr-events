b1110c9fc82ee50d0fd877cd6df43e9db2aacf71
Khushboo1008 <2019khushboo.dalwani@ves.ac.in>
2023-03-03 16:26:21 +0530

    fix: added toggle button on input for password visibility

--- src/containers/verify/components/Login.tsx
+++ src/containers/verify/components/Login.tsx
@@ -5,8 +5,11 @@ import { setStep } from '@/redux/verify'
 import { STATIC_PASSWORD } from '@/utils/constants'
 import { STEPS } from '../constants'
 import { ethers } from 'ethers'
+import { EyeOpen, EyeSlashed } from 'akar-icons'
 
 const Login = () => {
+  const [passwordType, setPasswordType] = useState('password')
+
   const [password, setPassword] = useState('')
   const dispatch = useAppDispatch()
 
@@ -27,6 +30,14 @@ const Login = () => {
   const changeStep = () => {
     dispatch(setStep(STEPS.QRSCAN))
   }
+
+  const togglePassword = () => {
+    if (passwordType === 'password') {
+      setPasswordType('text')
+      return
+    }
+    setPasswordType('password')
+  }
   return (
     <div className="w-screen p-10">
       <div className="mb-6">
@@ -36,14 +47,26 @@ const Login = () => {
         >
           Enter Password
         </label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
-          required
-        ></input>
+        <div className="relative">
+          <input
+            type={passwordType}
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 "
+            required
+          ></input>
+          <button
+            className="absolute top-0 right-0 rounded-r-lg border border-gray-300  p-1.5 text-sm font-medium text-white"
+            onClick={togglePassword}
+          >
+            {passwordType === 'password' ? (
+              <EyeSlashed strokeWidth={2} size={28} color="gray" />
+            ) : (
+              <EyeOpen strokeWidth={2} size={28} color="gray" />
+            )}
+          </button>
+        </div>
       </div>
 
       <button
