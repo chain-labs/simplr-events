2770a046dfccfaad593eb4f3f54b2b37035d9a9e
Pratham <chudasamapratham142@gmail.com>
2024-12-23 02:20:00 +0530

    added imagekit support for images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["ik.imagekit.io"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
