7bda13477c72590c6babd70b5f2f88250793d8b1
Pratham <chudasamapratham142@gmail.com>
2024-12-23 02:21:56 +0530

    finalizing the link-your-ticket page

---
+++ src/app/link-your-ticket/page.tsx
@@ -0,0 +1,15 @@
+import FooterProgressBar from "@/components/link-your-ticket/footer-progress-bar";
+import LinkedAndVerifiedTicket from "@/components/link-your-ticket/linked-and-verified-ticket";
+import LinkingTicketContainer from "@/components/link-your-ticket/linking-ticket-container";
+import OrderDetails from "@/components/link-your-ticket/order-details";
+
+export default function LinkYourTicket() {
+  return (
+    <>
+      {/* <LinkedAndVerifiedTicket /> */}
+      {/* <OrderDetails /> */}
+      <LinkingTicketContainer />
+      <FooterProgressBar />
+    </>
+  );
+}
