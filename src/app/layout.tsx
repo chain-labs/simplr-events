50773af54902b581b4ff9a72cc9c027046cdd1aa
Pratham <chudasamapratham142@gmail.com>
2024-12-24 01:01:46 +0530

    using kebab case

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import Script from "next/script";
 
-import Header from "@/components/Header";
+import Header from "@/components/header";
 
 import "./globals.css";
 
