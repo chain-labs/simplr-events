72f48751a862bcbd65ab58eafb5734114ca4ff8f
Pratham <chudasamapratham142@gmail.com>
2024-12-24 01:02:15 +0530

    updated with new dummydata

--- src/app/link-your-ticket/page.tsx
+++ src/app/link-your-ticket/page.tsx
@@ -1,15 +1,23 @@
-import FooterProgressBar from "@/components/link-your-ticket/footer-progress-bar";
+import FooterProgressBar, { StepsType } from "@/components/footer-progress-bar";
 import LinkedAndVerifiedTicket from "@/components/link-your-ticket/linked-and-verified-ticket";
 import LinkingTicketContainer from "@/components/link-your-ticket/linking-ticket-container";
 import OrderDetails from "@/components/link-your-ticket/order-details";
 
 export default function LinkYourTicket() {
+  const footerSteps: StepsType[] = [
+    { name: "Share your booking details", status: "completed" },
+    {
+      name: "Review your booking",
+      status: "active",
+    },
+    { name: "Start selling your ticket", status: "pending" },
+  ];
   return (
     <>
       {/* <LinkedAndVerifiedTicket /> */}
       {/* <OrderDetails /> */}
       <LinkingTicketContainer />
-      <FooterProgressBar />
+      <FooterProgressBar STEPS={footerSteps} />
     </>
   );
 }
