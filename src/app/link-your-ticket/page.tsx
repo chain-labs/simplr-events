8546ce08ebeccf8259e7b0942d638b4cb7e62098
Pratham <chudasamapratham142@gmail.com>
2024-12-26 03:07:42 +0530

    added centralized data

--- src/app/link-your-ticket/page.tsx
+++ src/app/link-your-ticket/page.tsx
@@ -1,7 +1,48 @@
+"use client";
+
+import { useState } from "react";
+
+import { z } from "zod";
+
 import FooterProgressBar, { StepsType } from "@/components/footer-progress-bar";
 import LinkedAndVerifiedTicket from "@/components/link-your-ticket/linked-and-verified-ticket";
 import LinkingTicketContainer from "@/components/link-your-ticket/linking-ticket-container";
 import OrderDetails from "@/components/link-your-ticket/order-details";
+import { Order } from "@/types/ticket";
+import { cn } from "@/utils/cn";
+import { dummyTickets } from "@/utils/dummyData";
+
+// Define the schema using zod
+export const linktingTicketFormSchema = z.object({
+  event: z.string().nonempty("Event is required"),
+  orderId: z.string().nonempty("Order ID is required"),
+  seatNumber: z.string().nonempty("Seat number is required"),
+});
+
+export type LinkingTicketFormData = z.infer<typeof linktingTicketFormSchema>;
+
+type centralizeStateData = {
+  event: string;
+  orderId: string;
+  seatNumber: string;
+  order: Order;
+};
+
+const emptyState: centralizeStateData = {
+  event: "",
+  orderId: "",
+  seatNumber: "",
+  order: {
+    orderId: "",
+    tickets: [],
+    startDate: "",
+    startTime: "",
+    endDate: "",
+    endTime: "",
+    eventName: "",
+    other: "",
+  },
+};
 
 export default function LinkYourTicket() {
   const footerSteps: StepsType[] = [
@@ -12,11 +53,75 @@ export default function LinkYourTicket() {
     },
     { name: "Start selling your ticket", status: "pending" },
   ];
+
+  const [state, setState] = useState<
+    "linking-ticket" | "order-details" | "link-and-verify-ticket"
+  >("linking-ticket");
+
+  const [centralizeState, setCentralizeState] =
+    useState<centralizeStateData>(emptyState);
+
+  const handleLinkingTicketSubmit = (data: LinkingTicketFormData) => {
+    // TODO: Replace dummyTickets with real data
+    const fetchingOrderData = dummyTickets.splice(0, 2);
+
+    setCentralizeState((prev) => ({
+      ...prev,
+      ...data,
+      order: {
+        ...prev.order,
+        // TODO: Replace dummyTickets with real data
+        ...fetchingOrderData[0],
+        tickets: fetchingOrderData,
+      },
+    }));
+
+    setState("order-details");
+  };
+
+  const navigation = (() => {
+    let currentState:
+      | "linking-ticket"
+      | "order-details"
+      | "link-and-verify-ticket" = "linking-ticket";
+
+    const next = () => {
+      if (currentState === "linking-ticket") {
+        currentState = "order-details";
+      } else if (currentState === "order-details") {
+        currentState = "link-and-verify-ticket";
+      }
+      setState(currentState);
+    };
+
+    const back = () => {
+      if (currentState === "order-details") {
+        currentState = "linking-ticket";
+      } else if (currentState === "link-and-verify-ticket") {
+        currentState = "order-details";
+      }
+      setState(currentState);
+    };
+
+    return { next, back };
+  })();
+
   return (
     <>
-      {/* <LinkedAndVerifiedTicket /> */}
-      {/* <OrderDetails /> */}
-      <LinkingTicketContainer />
+      <div
+        className={cn(
+          state === "linking-ticket" ? "block" : "hidden",
+          "h-full w-full"
+        )}
+      >
+        <LinkingTicketContainer
+          handleLinkingTicketSubmit={handleLinkingTicketSubmit}
+        />
+      </div>
+      {state === "order-details" && (
+        <OrderDetails navigation={navigation} data={centralizeState.order} />
+      )}
+      {state === "link-and-verify-ticket" && <LinkedAndVerifiedTicket />}
       <FooterProgressBar STEPS={footerSteps} />
     </>
   );
