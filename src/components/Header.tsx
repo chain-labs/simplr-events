51f265ee3b62d2e6bb3bc1e0f0a349eafd5023c6
Pratham <chudasamapratham142@gmail.com>
2024-12-26 03:08:49 +0530

    added wallet

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,18 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+
+import { PiCaretDown, PiWalletDuotone } from "react-icons/pi";
+
+import { cn } from "@/utils/cn";
 
 import { Button } from "./ui/button";
+import { LabelSmall } from "./ui/label";
+import { PMedium } from "./ui/paragraph";
 
 export default function Header() {
+  const [walletModelOpen, setWalletModelOpen] = useState(false);
   const Links = [
     { name: "home", href: "/" },
     { name: "link your ticket", href: "/link-your-ticket" },
@@ -13,8 +21,13 @@ export default function Header() {
     { name: "sell", href: "/sell-your-ticket" },
   ];
 
+  const account = {
+    isConnected: false,
+    wallet: "$250,000.0000",
+  };
+
   return (
-    <header className="sticky top-0 mx-auto h-fit w-full max-w-[1280px] bg-simpleBlue px-[32px] py-[16px]">
+    <header className="sticky top-0 z-50 mx-auto h-fit w-full max-w-[1280px] bg-simpleBlue px-[32px] py-[16px]">
       <nav className="flex w-full items-center justify-between">
         <div>
           <Image
@@ -39,13 +52,56 @@ export default function Header() {
             </li>
           ))}
         </ul>
+        <div className="relative">
+          <Button
+            variant="ghost"
+            className="relative flex items-center gap-[4px] text-[16px] leading-[20.8px] tracking-[0.02em]"
+            onClick={
+              walletModelOpen
+                ? () => setWalletModelOpen(false)
+                : () => setWalletModelOpen(true)
+            }
+          >
+            {/* @ts-expect-error */}
+            <PiWalletDuotone />
+            {account.wallet}
+            {
+              // @ts-expect-error
+              <PiCaretDown
+                className={cn(
+                  "transform transition-transform duration-300",
+                  walletModelOpen ? "rotate-180" : "rotate-0"
+                )}
+              />
+            }
+          </Button>
+          {walletModelOpen && (
+            <div className="absolute left-1/2 top-full flex -translate-x-1/2 flex-col items-center justify-center gap-[16px] rounded-[24px] bg-[#FFFFFF03] bg-simpleWhite p-[16px] shadow-[inset_2px_4px_4px_#FAFFD3BF,_inset_-2px_-4px_4px_#63680040,_inset_0_0_0_2px_#ffffff]">
+              <div className="flex flex-col gap-[8px] whitespace-nowrap text-left text-simpleGray700">
+                <LabelSmall>Your wallet Balance:</LabelSmall>
+                <PMedium className="text-[20px] font-bold leading-[26px] tracking-[0.02em]">
+                  {account.wallet}
+                </PMedium>
+              </div>
+              <Button variant="primary" size="sm">
+                withdraw balance
+              </Button>
+            </div>
+          )}
+        </div>
         <div className="flex items-center justify-center gap-2">
           <Button variant="outline" size="sm">
             contact us
           </Button>
-          <Button variant="outline" size="sm">
-            log out
-          </Button>
+          {account.isConnected ? (
+            <Button variant="outline" size="sm">
+              log out
+            </Button>
+          ) : (
+            <Button variant="primary" size="sm">
+              sign in
+            </Button>
+          )}
         </div>
       </nav>
     </header>
