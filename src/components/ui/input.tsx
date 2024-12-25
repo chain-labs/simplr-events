121b296272bcb7196fb213b7b27f3bde3e694a2c
Pratham <chudasamapratham142@gmail.com>
2024-12-26 03:08:33 +0530

    added valid field for input css

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -6,18 +6,22 @@ export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ReactNode;
   iconPosition?: "left" | "right";
+  parentClassName?: string;
+  valid?: boolean;
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, icon, iconPosition = "right", ...props }, ref) => {
     return (
-      <div className="relative">
+      <div className={cn("relative h-fit", props.parentClassName)}>
         <input
           type={type}
           className={cn(
-            "border-1 border-simpleGray300 placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-[8px] border bg-white px-[14px] py-[10px] font-switzer text-sm shadow-[0_1px_2px_#1018280D] ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            "border-1 placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-[8px] border border-simpleGray300 bg-white px-[14px] py-[10px] font-switzer text-sm text-simpleGray900 shadow-[0_1px_2px_#1018280D] ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
             icon && iconPosition === "left" && "pl-[42px]",
             icon && iconPosition === "right" && "pr-[42px]",
+            props.valid &&
+              "border-[#D6BBFB] text-simpleGray900 shadow-[0_0_0_4px_#9E77ED3D]",
             className
           )}
           ref={ref}
@@ -26,7 +30,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         {icon && (
           <div
             className={cn(
-              "text-simpleGray500 absolute top-0 flex h-full items-center text-[20px] leading-[20px]",
+              "absolute top-0 flex h-full items-center text-[20px] leading-[20px] text-simpleGray500",
               iconPosition === "left" && "left-[14px]",
               iconPosition === "right" && "right-[14px]"
             )}
