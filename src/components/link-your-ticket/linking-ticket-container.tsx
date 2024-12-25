97fc12ace1d61cf1df289d0d117301bd427a23ac
Pratham <chudasamapratham142@gmail.com>
2024-12-26 03:10:22 +0530

    tigheter checks

--- src/components/link-your-ticket/linking-ticket-container.tsx
+++ src/components/link-your-ticket/linking-ticket-container.tsx
@@ -5,24 +5,24 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { PiLinkDuotone, PiQuestion } from "react-icons/pi";
 import { z } from "zod";
 
+import {
+  LinkingTicketFormData,
+  linktingTicketFormSchema,
+} from "@/app/link-your-ticket/page";
 import { dummyTickets } from "@/utils/dummyData";
 
 import { Button } from "../ui/button";
 import { H4 } from "../ui/heading";
 import { Input } from "../ui/input";
+import TitleTag from "../ui/title-tag";
 import EventCardComponent from "./event-card-component";
 import EventSearchComponent from "./event-search-component";
 
-// Define the schema using zod
-const schema = z.object({
-  event: z.string().nonempty("Event is required"),
-  orderID: z.string().nonempty("Order ID is required"),
-  seatNumber: z.string().nonempty("Seat number is required"),
-});
-
-type FormData = z.infer<typeof schema>;
-
-export default function LinkingTicketContainer() {
+export default function LinkingTicketContainer({
+  handleLinkingTicketSubmit,
+}: {
+  handleLinkingTicketSubmit: (data: LinkingTicketFormData) => void;
+}) {
   // TODO: Replace dummyTickets with real data
   const suggestedEvents = dummyTickets.slice(0, 2);
 
@@ -30,17 +30,13 @@ export default function LinkingTicketContainer() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<FormData>({
-    resolver: zodResolver(schema),
+  } = useForm<LinkingTicketFormData>({
+    resolver: zodResolver(linktingTicketFormSchema),
   });
-
-  const onSubmit: SubmitHandler<FormData> = (data) => {
-    console.log(data);
-  };
 
   return (
     <form
-      onSubmit={handleSubmit(onSubmit)}
+      onSubmit={handleSubmit(handleLinkingTicketSubmit)}
       className="m-auto flex max-w-[780px] gap-[64px] rounded-bl-[16px] rounded-tr-[16px] bg-simpleWhite p-[48px]"
     >
       <div className="flex w-full flex-col gap-[16px]">
@@ -50,44 +46,58 @@ export default function LinkingTicketContainer() {
         <label className="flex flex-col gap-[8px] text-[14px] font-medium leading-[20px] text-simpleGray500">
           Choose your event
           <EventSearchComponent register={register("event")} />
-          {errors.event && (
-            <p className="text-[12px] text-red-500">{errors.event.message}</p>
-          )}
         </label>
         <label className="flex flex-col gap-[8px] text-[14px] font-medium leading-[20px] text-simpleGray500">
           Enter your Order ID
           <Input
             placeholder="OD123456789"
             icon={
-              //@ts-expect-error
-              <PiQuestion />
+              <TitleTag title="Order ID" position="bottom">
+                {/* @ts-expect-error */}
+                <PiQuestion />
+              </TitleTag>
             }
             iconPosition="right"
-            {...register("orderID")}
+            {...register("orderId", {
+              onChange: (e) => {
+                e.target.setCustomValidity("");
+              },
+            })}
+            onInvalid={(e: React.FormEvent<HTMLInputElement>) => {
+              if (errors.orderId) {
+                e.currentTarget.setCustomValidity(errors.orderId.message || "");
+              }
+            }}
+            required
           />
-          {errors.orderID && (
-            <p className="text-[12px] text-red-500">{errors.orderID.message}</p>
-          )}
-          <p className="text-[14px] font-normal leading-[20px] text-simpleGray600">
-            This is a hint text to help user.
-          </p>
         </label>
         <label className="flex flex-col gap-[8px] text-[14px] font-medium leading-[20px] text-simpleGray500">
           Enter your seat number
           <Input
             placeholder="Seat A123"
             icon={
-              // @ts-expect-error
-              <PiQuestion />
+              <TitleTag title="Seat Number" position="bottom">
+                {/* @ts-expect-error */}
+                <PiQuestion />
+              </TitleTag>
             }
             iconPosition="right"
-            {...register("seatNumber")}
+            {...register("seatNumber", {
+              onChange: (e) => {
+                e.target.setCustomValidity("");
+              },
+            })}
+            onInvalid={(e: React.FormEvent<HTMLInputElement>) => {
+              if (errors.seatNumber) {
+                e.currentTarget.setCustomValidity(
+                  errors.seatNumber.message || ""
+                );
+              } else {
+                e.currentTarget.setCustomValidity("");
+              }
+            }}
+            required
           />
-          {errors.seatNumber && (
-            <p className="text-[12px] text-red-500">
-              {errors.seatNumber.message}
-            </p>
-          )}
           <p className="text-[14px] font-normal leading-[20px] text-simpleGray600">
             This is a hint text to help user.
           </p>
