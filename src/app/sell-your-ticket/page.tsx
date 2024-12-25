c3e22c6b9db1bc001821e9ceba5db371b0a335b1
Pratham <chudasamapratham142@gmail.com>
2024-12-26 03:07:52 +0530

    added 2 components

--- src/app/sell-your-ticket/page.tsx
+++ src/app/sell-your-ticket/page.tsx
@@ -1,5 +1,11 @@
+"use client";
+
+import { useState } from "react";
+
 import FooterProgressBar, { StepsType } from "@/components/footer-progress-bar";
 import SellingLinkedTicket from "@/components/sell-your-ticket/selling-linked-ticket";
+import SettingTicketPricing from "@/components/sell-your-ticket/setting-ticket-pricing";
+import { Ticket } from "@/types/ticket";
 
 export default function LinkYourTicket() {
   const footerSteps: StepsType[] = [
@@ -19,9 +25,26 @@ export default function LinkYourTicket() {
       description: "Start collaborating with your team",
     },
   ];
+
+  const [selectedTickets, setSelectedTickets] = useState<Ticket[]>([]);
+
+  const [state, setState] = useState<
+    "selling-linked-ticket" | "setting-ticket-pricing"
+  >("setting-ticket-pricing");
   return (
     <>
-      <SellingLinkedTicket />
+      {state === "selling-linked-ticket" && (
+        <SellingLinkedTicket
+          selectedTickets={selectedTickets}
+          setSelectedTickets={setSelectedTickets}
+        />
+      )}
+      {state === "setting-ticket-pricing" && (
+        <SettingTicketPricing
+          selectedTickets={selectedTickets}
+          setSelectedTickets={setSelectedTickets}
+        />
+      )}
       <FooterProgressBar STEPS={footerSteps} />
     </>
   );
